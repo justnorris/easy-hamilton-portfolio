6983159042669d4991cabb656b9468c572cf134e
Norris <im@justnorris.com>
2017-10-30 10:13:34 +0200

    Fix textdomain

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="site-footer section-inner">
 
 	<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
-	<p class="site-footer__by"><?php esc_html_e( 'Theme by', 'easy-hamilton-portfolio' ); ?> <a href="http://www.andersnoren.se">Anders Nor&eacute;n</a> and <a href="http://www.colormelon.com">Colormelon</a></p>
+	<p class="site-footer__by"><?php esc_html_e( 'Theme by', 'minimalist-portfolio' ); ?> <a href="http://www.andersnoren.se">Anders Nor&eacute;n</a> and <a href="http://www.colormelon.com">Colormelon</a></p>
 
 </footer> <!-- footer -->
 
