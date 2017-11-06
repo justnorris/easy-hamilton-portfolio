00652be16cc8c75993d7d0971c0fc556adc333c5
Norris <im@justnorris.com>
2017-11-06 13:48:33 +0200

    Recommend Easy Photography Portfolio plugin

--- functions.php
+++ functions.php
@@ -7,20 +7,25 @@ function minimalist_portfolio_enqueue() {
 		return;
 	}
 
-	wp_enqueue_style( 'hamilton-style', get_template_directory_uri() . '/style.css', array( 'hamilton-fonts' ) );
+	wp_enqueue_style( 'hamilton-style', get_template_directory_uri() . '/style.css', [ 'hamilton-fonts' ] );
 	wp_enqueue_style( 'minimalist-portfolio-style', get_stylesheet_uri() );
 
-	wp_dequeue_script('masonry');
-	wp_enqueue_script('jquery-masonry');
+	wp_dequeue_script( 'masonry' );
+	wp_enqueue_script( 'jquery-masonry' );
 }
 
 add_action( 'wp_enqueue_scripts', 'minimalist_portfolio_enqueue', 100 );
 
 
-
 /*
  * Easy Photography Portfolio Config
  */
-if( class_exists('Colormelon_Photography_Portfolio')) {
-	require_once get_stylesheet_directory() . '/easy-photography-portfolio.php';
+$__DIR = get_stylesheet_directory();
+
+if ( class_exists( 'Colormelon_Photography_Portfolio' ) ) {
+	require_once $__DIR . '/inc/easy-photography-portfolio.php';
+}
+else {
+	require $__DIR . '/inc/plugins/tgm_plugin_activation.php';
+	require $__DIR . '/inc/plugins/recommend_plugins.php';
 }
