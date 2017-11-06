ae6e008592dca13fc5f933a3684face67abdc75c
Norris <im@justnorris.com>
2017-11-06 12:25:12 +0200

    Consistent prefixes

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
 
 
-function easy_minimalist_portfolio_enqueue() {
+function minimalist_portfolio_enqueue() {
 
 	if ( is_admin() ) {
 		return;
 	}
 
 	wp_enqueue_style( 'hamilton-style', get_template_directory_uri() . '/style.css', array( 'hamilton-fonts' ) );
-	wp_enqueue_style( 'easy-hamilton-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'minimalist-portfolio-style', get_stylesheet_uri() );
 }
 
-add_action( 'wp_enqueue_scripts', 'easy_minimalist_portfolio_enqueue', 100 );
+add_action( 'wp_enqueue_scripts', 'minimalist_portfolio_enqueue', 100 );
 
 
 
